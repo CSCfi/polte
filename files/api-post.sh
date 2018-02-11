b1a11414fd21afe5670f9a2235c1b271d3d6ea7f
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-02-11 20:39:34 +0200

    Add rest

---
+++ files/api-post.sh
@@ -0,0 +1,23 @@
+#!/bin/bash +xe
+
+#env
+source ansible_shell_env.sh
+
+#ssh agent
+ssh-agent -k
+eval $(ssh-agent -s)
+ssh-add "$OS_KEY_FILE"
+
+#run
+ansible-playbook -i inventory playbooks/post_puppetize_apis.yml -e "puppet_environment=$PUPPET_ENVIRONMENT"
+
+#cleanup
+if [ $? -eq 0 ]; then
+  openstack stack delete MULTIBRANCH_"$PUPPET_ENVIRONMENT"
+  rm inventory/ooo_inventory
+  ssh-agent -k
+  exit 0
+else
+  ssh-agent -k
+  exit 1
+fi
