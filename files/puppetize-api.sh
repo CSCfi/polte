8d53407e53c4cc87c3f9d9494d5236123dea7619
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-02-11 20:33:57 +0200

    Add Ceph and APIs

---
+++ files/puppetize-api.sh
@@ -0,0 +1,25 @@
+#!/bin/bash +xe
+TIMEOUT=1200
+
+#env
+source ansible_shell_env.sh
+
+#ssh agent
+ssh-agent -k
+eval $(ssh-agent -s)
+ssh-add "$OS_KEY_FILE"
+
+#run
+timeout $TIMEOUT ansible-playbook -i inventory playbooks/puppetize_apis_loop.yml -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true"
+if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
+timeout $TIMEOUT ansible-playbook -i inventory playbooks/puppetize_apis_loop.yml -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true"
+if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
+timeout $TIMEOUT ansible-playbook -i inventory playbooks/puppetize_apis_loop.yml -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true"
+if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
+timeout $TIMEOUT ansible-playbook -i inventory playbooks/puppetize_apis_loop.yml -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true"
+if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
+timeout $TIMEOUT ansible-playbook -i inventory playbooks/puppetize_apis_loop.yml -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true"
+if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
+
+#cleanup
+[ $? -eq 0 ] && ssh-agent -k
