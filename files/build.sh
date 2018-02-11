cf00bbd92d18d92e572feac36c06d393ee19f0d9
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-02-11 18:54:20 +0200

    Reschedule role installation

--- files/build.sh
+++ files/build.sh
@@ -8,6 +8,10 @@ ssh-agent -k
 eval $(ssh-agent -s)
 ssh-add "$OS_KEY_FILE"
 
+#ansible roles
+mkdir -p roles
+ansible-galaxy install -r requirements.yml --roles-path roles/
+
 #run
 ansible-playbook playbooks/build-heat-stack.yml \
 -i inventory \
