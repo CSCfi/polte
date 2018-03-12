be013c3d3bbf3799d1d2d42990381d9cd8430eea
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-03-12 08:04:12 +0200

    Remove timeouts
    Remove timeouts which are not needed anymore in API node puppetize
    loop.

--- files/puppetize-obj.sh
+++ files/puppetize-obj.sh
@@ -1,5 +1,4 @@
 #!/bin/bash +xe
-TIMEOUT=1200
 
 #env
 source ansible_shell_env.sh
@@ -10,9 +9,9 @@ eval $(ssh-agent -s)
 ssh-add "$OS_KEY_FILE"
 
 #run
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_objs.yml
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_objs.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_objs.yml
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_objs.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
 
 #cleanup
