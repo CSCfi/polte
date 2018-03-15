0a251e2caed2886e843cf0916ca5238837833189
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-03-15 12:50:26 +0200

    Addt'l condition for failure

--- files/puppetmaster.sh
+++ files/puppetmaster.sh
@@ -12,6 +12,7 @@ ssh-add "$OS_KEY_FILE"
 ansible-playbook -i inventory \
 -e "puppet_environment=$PUPPET_ENVIRONMENT" \
 playbooks/puppetmaster.yml
+if [ ! $? -eq 0 ]; then ssh-agent -k && exit 1; fi
 
 ansible-playbook -i inventory \
 playbooks/generate_hostsfile.yml \
