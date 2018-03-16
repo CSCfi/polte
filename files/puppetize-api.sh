17d886148287f1bb965547acf11cb78ce4fcd9b6
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-03-16 21:47:02 +0200

    Remove loopfail variable

--- files/puppetize-api.sh
+++ files/puppetize-api.sh
@@ -10,15 +10,15 @@ eval $(ssh-agent -s)
 ssh-add "$OS_KEY_FILE"
 
 #run
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true" playbooks/puppetize_apis_loop.yml
+timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true" playbooks/puppetize_apis_loop.yml
+timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true" playbooks/puppetize_apis_loop.yml
+timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true" playbooks/puppetize_apis_loop.yml
+timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 if [ $? -eq 0 ]; then ssh-agent -k && exit 0; fi
-timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT loopfail=true" playbooks/puppetize_apis_loop.yml
+timeout $TIMEOUT ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 
 #cleanup
 [ $? -eq 0 ] && ssh-agent -k
