41b5b3d25150476ff855015f4be7c332a99536b7
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-04-09 21:29:29 +0300

    Readd stack delete and normal hiera

--- files/api-post.sh
+++ files/api-post.sh
@@ -8,8 +8,8 @@ ansible-playbook -i inventory playbooks/post_puppetize_apis.yml
 
 #cleanup
 if [ $? -eq 0 ]; then
-#  openstack stack delete MULTIBRANCH_"$PUPPET_ENVIRONMENT"
-#  rm inventory/ooo_inventory
+  openstack stack delete MULTIBRANCH_"$PUPPET_ENVIRONMENT"
+  rm inventory/ooo_inventory
   exit 0
 fi
 exit 1
