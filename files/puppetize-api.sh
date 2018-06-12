c8c192134fa9c560822e88581713a3d26a2d3e27
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-06-12 15:53:48 +0300

    Remove opsviewagent and nagios workarounds
    More retries in API builds.

--- files/puppetize-api.sh
+++ files/puppetize-api.sh
@@ -13,3 +13,11 @@ if [ $? -eq 0 ]; then exit 0; fi
 ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
 if [ $? -eq 0 ]; then exit 0; fi
 ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
+if [ $? -eq 0 ]; then exit 0; fi
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
+if [ $? -eq 0 ]; then exit 0; fi
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
+if [ $? -eq 0 ]; then exit 0; fi
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
+if [ $? -eq 0 ]; then exit 0; fi
+ansible-playbook -i inventory -e "puppet_environment=$PUPPET_ENVIRONMENT" playbooks/puppetize_apis_loop.yml
