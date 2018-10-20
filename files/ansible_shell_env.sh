2f19bb7321b663471225750e4179311ebee961f4
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2018-10-20 18:19:04 +0300

    Remove duplicate variable

--- files/ansible_shell_env.sh
+++ files/ansible_shell_env.sh
@@ -12,14 +12,14 @@ cp /var/lib/jenkins/credentials/heat-params.yml files/heat-params.yml
 #shell environment variables
 source "{{ openrc_location }}"
 export OS_PASSWORD=$(cat "{{ os_password_location }}")
-export OS_KEY_FILE="{{ os_key_file_location }}"
+export ANSIBLE_KEY_FILE="{{ ansible_ssh_private_key_file }}"
 export VAULT_PASS_FILE="{{ vault_pass_file_location }}"
 export HIERA_PRIVATE_KEY="$(cat "{{ hiera_private_key_location }}")
 export HIERA_PUBLIC_KEY="$(cat "{{ hiera_public_key_location }}")
 
 #ansible cfg
 echo "vault_password_file=$VAULT_PASS_FILE" >> ansible.cfg
-echo "ansible_ssh_private_key_file=$OS_KEY_FILE" >> ansible.cfg
+echo "ansible_ssh_private_key_file=$ANSIBLE_KEY_FILE" >> ansible.cfg
 
 #puppet env
 source puppet_env.sh
