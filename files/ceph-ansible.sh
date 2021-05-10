b6bb8aaa59d69c769234ea5b1b25d6e92e9e073f
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2021-05-10 11:15:05 +0300

    Re-oops

--- files/ceph-ansible.sh
+++ files/ceph-ansible.sh
@@ -22,7 +22,7 @@ python -m pip install 'netaddr'
 
 
 #prereq
-ansible -m shell -a "sudo yum install -y centos-release-ceph-nautilus" -i inventory "mons,osds,mgrs" --vault-password-file="$VAULT_PASS_FILE"
+ansible -m shell -a "sudo yum install -y centos-release-ceph-luminous" -i inventory "mons,osds,mgrs" --vault-password-file="$VAULT_PASS_FILE"
 cp /var/lib/jenkins/credentials/vault-mons.yml inventory/group_vars/mons/vault.yml
 cp /var/lib/jenkins/credentials/vault-osds.yml inventory/group_vars/osds/vault.yml
 
